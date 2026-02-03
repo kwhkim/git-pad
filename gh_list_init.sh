bf28bbf0bb9f415d707e743dd40991c03cf96f86
kwhk_gogpu@gangdong <kwonhyun.kim@gmail.com>
2026-02-03 21:44:23 +0900

    gh_list_init.sh| pritn db info before delete

--- gh_list_init.sh
+++ gh_list_init.sh
@@ -3,7 +3,32 @@
 # shellcheck source=/dev/null
 source gh_list_vars.sh
 
-cat > "$DB_STATE_FILE" <<'EOF'
+gh_repo_createdAt() {
+  # shellcheck disable=SC2016
+  gh api graphql -f query='
+    query($o:String!,$r:String!){
+    repository(owner:$o,name:$r){ createdAt }
+    }' -F o="$1" -F r="$2" | jq -r '.data.repository.createdAt'
+}
+
+echo "- repo created at: $(gh_repo_createdAt "$OWNER" "$REPO")"
+
+sqlite3 -box "$DB_FILE" << EOF
+SELECT
+  MIN(updated_at) AS min_updated_at,
+  MAX(updated_at) AS max_updated_at,
+  COUNT(*)        AS row_count
+FROM issues;
+EOF
+
+read -p "- Press Y to initialize database $DB_FILE and state file $DB_STATE_FILE ..." ans
+
+if [[ "$ans" != "Y" && "$ans" != "y" ]]; then
+  echo "- Aborted."
+  exit 1
+fi
+
+cat > "$DB_STATE_FILE" << 'EOF'
 {
   "updated_at_min": "",
   "updated_at_max": "",
