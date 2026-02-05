291f7ff138a8e0845b06dcf1278fd550ff798585
kwhk_gogpu@gangdong <kwonhyun.kim@gmail.com>
2026-02-05 16:39:29 +0900

    gh_list.sh| print the lastest row

--- gh_list.sh
+++ gh_list.sh
@@ -5,3 +5,4 @@ source gh_list_vars.sh
 
 sqlite3 -box "$DB_FILE" "SELECT id,number,state,title,updated_at,labels,assignees FROM issues ORDER BY number DESC LIMIT 5;"
 sqlite3 -box "$DB_FILE" "SELECT id,number,state,title,updated_at,labels,assignees FROM issues ORDER BY updated_at DESC LIMIT 5;"
+sqlite3 -box "$DB_FILE" "SELECT * FROM issues ORDER BY updated_at DESC LIMIT 1;"
