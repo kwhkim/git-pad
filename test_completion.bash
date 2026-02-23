09f13c33a5d52f3f62a8cd880db1711ec7ed73df
kwhk_gogpu@gangdong <kwonhyun.kim@gmail.com>
2026-02-23 19:24:09 +0900

    test for git bash and bash| test.yml -> test_completion.bash

---
+++ test_completion.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Load completion environment
+source /usr/share/bash-completion/bash_completion
+source /usr/share/bash-completion/completions/git
+source ./autocompletion.bash
+
+# # Simulate:
+# # git pad edit P<TAB>
+# COMP_WORDS=(git pad edit P)
+# COMP_CWORD=3
+# COMP_LINE="git pad edit P"
+# COMP_POINT=${#COMP_LINE}
+# 
+# _git_pad
+# 
+# printf '%s\n' "${COMPREPLY[@]}"
+
+
+# Simulate:
+# git pad e<TAB>
+echo '- Testing git pad e<TAB>'
+COMP_WORDS=(git pad e)
+COMP_CWORD=2
+COMP_LINE="git pad e"
+COMP_POINT=${#COMP_LINE}
+
+_git_pad
+
+printf '%s\n' "${COMPREPLY[@]}"
